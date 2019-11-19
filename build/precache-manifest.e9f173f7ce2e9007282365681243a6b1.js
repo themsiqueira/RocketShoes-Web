a9249cbb0ae976f50f176c262f916b639747e23c
Mateus Siqueira Carneiro <mateus.carneiro@agrotools.local>
2019-11-19 14:01:35 -0300

    Add build to deploy

---
+++ build/precache-manifest.e9f173f7ce2e9007282365681243a6b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "980295b1fd014c915af2b166f0bb48ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87995290828c71da9dc6",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "87995290828c71da9dc6",
+    "url": "/static/js/2.782905e5.chunk.js"
+  },
+  {
+    "revision": "ef04bec5304692cc65a6",
+    "url": "/static/js/main.6a1a1288.chunk.js"
+  },
+  {
+    "revision": "5f2736ed5603b0c6ee57",
+    "url": "/static/js/runtime-main.baa79e65.js"
+  },
+  {
+    "revision": "3c9a1ce505a48f2c15ba9505b6f5667b",
+    "url": "/static/media/background.3c9a1ce5.svg"
+  },
+  {
+    "revision": "879557c5bdbbd3743a7680c651e31d08",
+    "url": "/static/media/logo.879557c5.svg"
+  }
+]);
